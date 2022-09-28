02b6453a4e5db075e309f16b2949e7b045270dd9
Joel <joel.v.paul@outlook.com>
2022-09-28 22:02:39 +1000

    Implement adding markers

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,23 +1,38 @@
 
 class Board
   SIZE = 3
-  
+
   EMPTY = ' '
   X = 'X'
   O = 'O'
 
   def initialize
     @board = Array.new(SIZE, EMPTY) { Array.new(SIZE, EMPTY) }
+    print_board
   end
 
-  private
+  def p1_place(x, y)
+    place_marker(X, x, y)
+  end
+
+  def p2_place(x, y)
+    place_marker(O, x, y)
+  end
 
   def print_board
     puts "   |   |   \n"
     puts @board.map { |row| " #{row.join(' | ')} " }.join("\n___|___|___\n   |   |   \n")
     puts "   |   |   \n"
   end
+
+  private
+
+  def place_marker(marker, x, y)
+    @board[y][x] = marker
+    print_board
+  end
 end
 
 board = Board.new
-board.print_board
+board.p1_place(0, 0)
+board.p2_place(1, 0)
