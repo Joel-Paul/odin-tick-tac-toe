6fd3374f14fea23c18e4cf77ad6e2ba0edcd87be
Joel <joel.v.paul@outlook.com>
2022-10-08 19:55:39 +1100

    Implement win checking

--- tictactoe.rb
+++ tictactoe.rb
@@ -11,25 +11,37 @@ class Board
   def initialize
     @board = Array.new(3, EMPTY) { Array.new(3, EMPTY) }
     @current_player = 1
-    print_board
   end
 
   def game_over?
-    false
+    won = @board.any? { |row| same_markers?(row) }
+    won ||= @board.transpose.any? { |row| same_markers?(row) }
+    won || game_over_diagonal?
   end
 
   def place_marker(x_pos, y_pos)
     @board[y_pos][x_pos] = @current_player == 1 ? PLAYER_1 : PLAYER_2
-    @current_player = @current_player % 2 + 1
-    print_board
+    @current_player = @current_player % 2 + 1 unless game_over?
   end
-
-  private
 
   def print_board
     puts "   |   |   \n"
     puts @board.map { |row| " #{row.join(' | ')} " }.join("\n___|___|___\n   |   |   \n")
     puts "   |   |   \n"
+  end
+
+  private
+
+  def game_over_diagonal?
+    diagonals = Array.new(2)
+    diagonals[0] = (0..2).map { |i| @board[i][i] }
+    diagonals[1] = (0..2).map { |i| @board[i][2 - i] }
+
+    diagonals.any? { |row| same_markers?(row) }
+  end
+
+  def same_markers?(row)
+    row.uniq.length == 1 && row[0] != EMPTY
   end
 end
 
@@ -40,7 +52,12 @@ class Game
   end
 
   def play
-    @board.place_marker(*process_input) until @board.game_over?
+    @board.print_board
+
+    until @board.game_over?
+      @board.place_marker(*process_input)
+      @board.print_board
+    end
   end
 
   def process_input
